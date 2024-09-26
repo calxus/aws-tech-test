9dd9fd10c6a85d54459e52dbb7eaa7a52d456e2b
Gordon Adam-Santiago <gtadam@protonmail.ch>
2024-09-26 10:36:13 +0100

    Add web application firewall

---
+++ infra/waf.tf
@@ -0,0 +1,36 @@
+resource "aws_wafv2_web_acl" "poc" {
+  name  = "poc-web-acl"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 0
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "poc-web-acl"
+    sampled_requests_enabled   = true
+  }
+}
